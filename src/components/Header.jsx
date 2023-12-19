9a21069aedcc64ac1782a4497ca4789cb933f561
bowen-anth <bowen.anth@gmail.com>
2023-12-19 09:16:30 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="https://www.linkedin.com/in/anthony-bowen-15856131/" target="_blank">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/bowen-anth" target="_blank">Github</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
